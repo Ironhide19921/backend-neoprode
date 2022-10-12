d72b038f4b05ba44c7a79823c2ebc501700a8ca1
Ironhide19921 <gera_lps@hotmail.com.ar>
2022-10-12 17:44:32 -0300

    first commit

---
+++ src/resolvers/mutation.ts
@@ -0,0 +1,7 @@
+import { IResolvers } from "@graphql-tools/utils";
+
+const mutationResolvers: IResolvers = {
+  Mutation: {},
+};
+
+export default mutationResolvers;
---
+++ src/resolvers/query.ts
@@ -0,0 +1,20 @@
+import { IResolvers } from "@graphql-tools/utils";
+import { IUser } from "../interfaces/user.interface";
+
+const queryResolvers: IResolvers = {
+  Query: {
+    users: (): Array<IUser> => {
+      return [
+        {
+          id: "1",
+          name: "Anartz",
+          lastname: "Mugika Ledo",
+          email: "",
+          registerDate: "",
+        },
+      ];
+    },
+  },
+};
+
+export default queryResolvers;
