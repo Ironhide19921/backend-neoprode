97d5b86de37159d6bd76fd21137c079214487247
Ironhide19921 <gera_lps@hotmail.com.ar>
2022-10-14 12:20:02 -0300

    Resolver add y query

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,7 +1,80 @@
+import { IUser } from "../interfaces/user.interface";
 import { IResolvers } from "@graphql-tools/utils";
+import { Db } from "mongodb";
 
 const mutationResolvers: IResolvers = {
-  Mutation: {},
+  Mutation: {
+    add: async (
+      _: void,
+      args: { user: IUser },
+      context: { db: Db }
+    ): Promise<{
+      status: boolean;
+      message: string;
+      user?: IUser;
+    }> => {
+      // Comprobar si existe el usuario en la base de datos con el correo
+      // Si existe, error mostrando feedback
+      const userCheck = await context.db
+        .collection("users")
+        .findOne({ email: args.user.email });
+
+      if (userCheck) {
+        return {
+          status: false,
+          message: "Usuario existe y no podemos registrarnos",
+        };
+      }
+
+      // Usuario sin password definido, mostrar error si se cumple
+      if (!args.user.password) {
+        return {
+          status: false,
+          message: "Password no establecido / asignado",
+        };
+      }
+
+      // YA TENEMOS LO NECESARIO PARA AÑADIR
+      // Si todo va bien y tenemos la información para almacenar
+      // Asignar el ID nuevo automáticamente, basándonos
+      const lastElement = await context.db
+        .collection("users")
+        .find()
+        .limit(1)
+        .sort({
+          registerDate: -1,
+        })
+        .toArray();
+
+      args.user.id =
+        lastElement.length === 0 ? "1" : String(+lastElement[0].id + 1);
+
+      console.log(args.user);
+
+      // Añadir la fecha de registro antes de almacenar
+      args.user.registerDate = new Date().toISOString();
+      // Encriptar password antes de almacenar
+
+      // Insertar el usuario en la base de datos
+      return await context.db
+        .collection("users")
+        .insertOne(args.user)
+        .then(() => {
+          return {
+            status: true,
+            message: "Añadido correctamente",
+            user: args.user,
+          };
+        })
+        .catch((error) => {
+          console.log(`ERROR: ${error}`);
+          return {
+            status: false,
+            message: `ERROR: ${error}`,
+          };
+        });
+    },
+  },
 };
 
 export default mutationResolvers;
